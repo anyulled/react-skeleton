9161b64eb5ce4b94da93cbef0b318c8f929bfd6c
infs <infs@gft.com>
2016-09-01 15:10:37 +0200

    css-loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
                 query: {
                     presets: ["es2015", "react"]
                 }
-            }
+            },
+            { test: /\.css$/, loader: "style-loader!css-loader" }
         ]
     },
     plugins: [
