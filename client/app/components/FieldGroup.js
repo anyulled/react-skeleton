5496b0cd565cf88fdb5bcac1f70cf1ad82df76f1
alrs <anyul.rivas@gft.com>
2016-09-15 15:24:08 +0200

    including propTypes

--- client/app/components/FieldGroup.js
+++ client/app/components/FieldGroup.js
@@ -2,8 +2,17 @@ import React from "react";
 import {danger} from "../utils/colors";
 import {FormControl, FormGroup, ControlLabel} from "react-bootstrap";
 
-export default ({field, label}) => (<FormGroup>
-    <ControlLabel htmlFor={field.name}>{label}</ControlLabel>
-    <FormControl type="text" className="u-full-width" {...field} />
-    {field.touched && field.error && <div style={{color: "white", backgroundColor: danger}}>{field.error}</div>}
-</FormGroup>).propTypes = {field: React.PropTypes.object.isRequired, label: React.PropTypes.string.isRequired};
+const FieldGroup = ({field, label}) => {
+    return (<FormGroup>
+        <ControlLabel htmlFor={field.name}>{label}</ControlLabel>
+        <FormControl type="text" className="u-full-width" {...field} />
+        {field.touched && field.error && <div style={{color: "white", backgroundColor: danger}}>{field.error}</div>}
+    </FormGroup>);
+};
+
+FieldGroup.propTypes = {
+    field: React.PropTypes.object.isRequired,
+    label: React.PropTypes.string.isRequired
+};
+
+export default FieldGroup;
