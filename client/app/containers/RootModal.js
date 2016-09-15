6b2c863067e1405c322be4689410b1603e7a3874
alrs <anyul.rivas@gft.com>
2016-09-15 10:20:11 +0200

    root modal

--- client/app/containers/RootModal.js
+++ client/app/containers/RootModal.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import { Modal } from "react-bootstrap";
-import EditUserModal from "./modals/EditUserModal";
-import NewUserModal from "./modals/NewUserModal";
-import { connect } from "react-redux";
+import React from "react";
+import EditUserModal from "./Modals/EditUserModal";
+import NewUserModal from "./Modals/NewUserModal";
+import {connect} from "react-redux";
 
 const MODAL_COMPONENTS = {
-  'NEW_USER': NewUserModal,
-  'EDIT_USER': EditUserModal
-  /* other modals */
-}
-
-const RootModal = ({ modalType, modalProps }) => {
-			if (!modalType) {
-				return null;
-			}
-
-			const SpecificModal = MODAL_COMPONENTS[modalType]
-
-			return (<SpecificModal show={Boolean(modalType)} {...modalProps} />);
-		};
-		
-const mapStateToProps = (state) => {
-	return {
-		modalType: state.ui.modal.modalType,
-		modalProps: state.ui.modal.modalProps
-	};
+    "NEW_USER": NewUserModal,
+    "EDIT_USER": EditUserModal
 };
-		 
+
+const RootModal = ({modalType, modalProps}) => {
+    if (!modalType) {
+        return null;
+    }
+
+    const SpecificModal = MODAL_COMPONENTS[modalType];
+
+    return (<SpecificModal show={Boolean(modalType)} {...modalProps} />);
+};
+
+RootModal.prototype = {
+    modalType: React.PropTypes.string,
+    modalProps: React.PropTypes.object
+};
+
+const mapStateToProps = (state) => {
+    return {
+        modalType: state.ui.modal.modalType,
+        modalProps: state.ui.modal.modalProps
+    };
+};
+
 
 export default connect(
-	mapStateToProps,
-	null
+    mapStateToProps,
+    null
 )(RootModal);
