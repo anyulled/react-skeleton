32adbbc1035a7500cc9800f8b3fa9fc9a30b1b78
alrs <anyul.rivas@gft.com>
2016-09-15 15:24:30 +0200

    catching ajax call errors

--- client/app/actions/generic/generic.js
+++ client/app/actions/generic/generic.js
@@ -4,6 +4,7 @@ import config from "../../config";
 export const ACTION_ONE = "action_one";
 export const ACTION_TWO = "action_two";
 export const ACTION_THREE = "action_three";
+export const ERROR = "error";
 
 export function actionOne(dto) {
     return {
@@ -26,10 +27,18 @@ export function actionThree(params = null) {
         dispatch({
             type: ACTION_THREE
         });
-        axios.get(config.api.url + "/mock/mock_data.json", {params}).then((data)=> {
-            dispatch({
-                type: ACTION_THREE
+        axios.get(config.api.url + "/mock/mock_data.json", {params})
+            .then((data)=> {
+                dispatch({
+                    type: ACTION_THREE,
+                    payload: data.data
+                });
+            })
+            .catch((error)=> {
+                dispatch({
+                    type: ERROR,
+                    payload: error
+                });
             });
-        });
     };
 }
