58edfe0464476ba0a1e09b9b2f0d2fa78ab06ff4
alrs <anyul.rivas@gft.com>
2016-09-15 10:14:39 +0200

    server

--- server/app.js
+++ server/app.js
@@ -6,17 +6,25 @@ var cors = require("cors");
 app.use(cors());
 app.use(express.static("static"));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", function (req, res) {
+    res.send("Hello World!");
 });
 
-app.get('/users', function (req, res) {
-	res.set({
-	  'Content-Type': 'application/json'
-	});
-  res.sendFile(__dirname + '/static/users.json');
+app.get("/users", function (req, res) {
+    res.set({
+        "Content-Type": "application/json"
+    });
+    res.sendFile(__dirname + "/static/users.json");
+});
+
+app.get("/deals", function (request, response) {
+    console.log("Deals requested:" + request);
+    response.set({
+        "Content-Type": "application/json"
+    });
+    response.sendfile(__dirname + "/static/deals.json");
 });
 
 app.listen(3000, function () {
-  console.log("App listening on port 3000!");
+    console.log("App listening on port 3000!");
 });
