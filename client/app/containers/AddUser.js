f9c6eababd374a13b17edc8645874255d4a9624a
alrs <anyul.rivas@gft.com>
2016-09-15 10:21:14 +0200

    including proptypes

--- client/app/containers/AddUser.js
+++ client/app/containers/AddUser.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import * as userActions from "../actions/users/users";
+import React from "react";
+import {connect} from "react-redux";
 import * as uiActions from "../actions/ui/ui";
-import { Button } from "react-bootstrap";
+import {Button} from "react-bootstrap";
 
-const AddUser = ({ onAddUserClick }) => { return (<Button type='button' bsStyle="info" onClick={() => onAddUserClick()}>
-					New User
-		</Button>);
+const AddUser = ({onAddUserClick}) => {
+    return (<Button type="button" bsStyle="info" onClick={() => onAddUserClick()}>
+        New User
+    </Button>);
 };
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		onAddUserClick: () => {
-			dispatch(uiActions.modalNewUser());
-		}
-	};
+    return {
+        onAddUserClick: () => {
+            dispatch(uiActions.modalNewUser());
+        }
+    };
 };
 
 const mapStateToProps = (state) => {
-	return {
-		modalType: state.ui.modal.modalType,
-		modalProps: state.ui.modal.modalProps
-	};
+    return {
+        modalType: state.ui.modal.modalType,
+        modalProps: state.ui.modal.modalProps
+    };
 };
-		 
+
+AddUser.propType = {
+    onAddUserClick: React.PropTypes.func.isRequired
+};
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(AddUser);
