2fd616167cb037a744c967a36d9e01b4f32c43d8
alrs <anyul.rivas@gft.com>
2016-09-15 10:21:51 +0200

    styling

--- client/app/components/FieldGroup.js
+++ client/app/components/FieldGroup.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { danger } from '../utils/colors';
-import { FormControl, FormGroup, ControlLabel } from "react-bootstrap";
- 
-export default ({field, label}) => (<FormGroup> 
-	 <ControlLabel htmlFor={field.name}>{label}</ControlLabel> 
-	 <FormControl type='text' className="u-full-width" {...field} /> 
-	 {field.touched && field.error && <div style={{color: 'white', backgroundColor: danger}}>{field.error}</div>}
-</FormGroup>);
+import React from "react";
+import {danger} from "../utils/colors";
+import {FormControl, FormGroup, ControlLabel} from "react-bootstrap";
+
+export default ({field, label}) => (<FormGroup>
+    <ControlLabel htmlFor={field.name}>{label}</ControlLabel>
+    <FormControl type="text" className="u-full-width" {...field} />
+    {field.touched && field.error && <div style={{color: "white", backgroundColor: danger}}>{field.error}</div>}
+</FormGroup>).propTypes = {field: React.PropTypes.object.isRequired, label: React.PropTypes.string.isRequired};
