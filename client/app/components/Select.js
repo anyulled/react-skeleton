afd56bf12ddef954ad3cf6cb505cdf6e4e422eb1
alrs <anyul.rivas@gft.com>
2016-09-15 10:20:31 +0200

    including proptypes

--- client/app/components/Select.js
+++ client/app/components/Select.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import { danger } from '../utils/colors';
-import { FormControl, FormGroup, ControlLabel } from "react-bootstrap";
+import React from "react"
+import {danger} from "../utils/colors";
+import {FormControl, FormGroup, ControlLabel} from "react-bootstrap";
 
-export default ({field, label, options, ...props}) => (<FormGroup>
-	<ControlLabel htmlFor={field.name}>{label}</ControlLabel>
-	<FormControl componentClass="select" {...field} {...props}>
-		{options.map(c => <option value={c.id} key={c.id} >{c.name}</option>)}
-	</FormControl>
-	{field.touched && field.error && <div style={{color: 'white', backgroundColor: danger}}>{field.error}</div>}
-</FormGroup>);
+const Select = ({field, label, options, ...props}) => {
+    return (<FormGroup>
+        <ControlLabel htmlFor={field.name}>{label}</ControlLabel>
+        <FormControl componentClass="select" {...field} {...props}>
+            {options.map(c => <option value={c.id} key={c.id}>{c.name}</option>)}
+        </FormControl>
+        {field.touched && field.error && <div style={{color: "white", backgroundColor: danger}}>{field.error}</div>}
+    </FormGroup>);
+};
+
+Select.proptypes = {
+    field: React.PropTypes.object.isRequired,
+    label: React.PropTypes.string.isRequired
+};
+
+export default Select;
