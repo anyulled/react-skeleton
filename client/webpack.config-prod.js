6b0121943ed93097c9fc1daff1822ab5b4c8aa0e
argc <argc@NBBCN958.gft.com>
2016-09-20 13:55:29 +0200

    changed the loader to babel-loader as mandated by npm

--- client/webpack.config-prod.js
+++ client/webpack.config-prod.js
@@ -21,7 +21,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: "babel",
+                loader: "babel-loader",
                 query: {
                     presets: ["es2015", "react", "stage-0"]
                 }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: "babel",
+                loader: "babel-loader",
                 query: {
                     presets: ["es2015", "react", "stage-0"]
                 }
