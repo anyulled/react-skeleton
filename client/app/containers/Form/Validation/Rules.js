40e2bc70c1dcf96b481935875d72dc0ad585f7af
argc <argc@NBBCN958.gft.com>
2016-10-21 13:30:50 +0200

    improvements and refactors on dynamic forms

--- client/app/containers/Form/Validation/Rules.js
+++ client/app/containers/Form/Validation/Rules.js
@@ -3,23 +3,49 @@ import * as types  from "./Types";
 export const requiredRule={
 	"name": "required",
 	"method": (evalValue)=>{
-		console.log(evalValue);
-		console.log(evalValue!=undefined && evalValue!=null && evalValue!="");
 		return (evalValue!=undefined && evalValue!=null && evalValue!="")?null:"Required";
 	}
 }
 
 export const minRule={
 	"name": "min",
-	"method": (evalValue, ruleValue, printableRuleValue)=>{//TODO ARGC use text depending on type (longer than, older than)
-		return (evalValue.length >= ruleValue)?null:("Please enter a value higher than " + printableRuleValue);
+	"method": (evalValue, ruleValue, printableRuleValue,dataType)=>{
+		let _evalValue,ruleMessage;
+		switch(dataType){
+			case types.TYPE_TEXT:
+				_evalValue=evalValue.length;
+				ruleMessage=`Please enter a text with a minimum of ${printableRuleValue} characters`;
+				break;
+			case types.TYPE_DATE:
+				_evalValue=evalValue;
+				ruleMessage=`Please enter a date later or equal to ${printableRuleValue}`;
+				break;
+			default:
+				_evalValue=evalValue;
+				ruleMessage=`Minimum allowed value is ${printableRuleValue}`;
+		}
+		return (_evalValue >= ruleValue)?null:ruleMessage;
 	}
 }
 
 export const maxRule={
 	"name": "max",
-	"method": (evalValue, ruleValue, printableRuleValue)=>{
-		return (evalValue.length <= ruleValue)?null:("Please enter a value lower than " + printableRuleValue);
+	"method": (evalValue, ruleValue, printableRuleValue,dataType)=>{
+		let _evalValue,ruleMessage;
+		switch(dataType){
+			case types.TYPE_TEXT:
+				_evalValue=evalValue.length;
+				ruleMessage=`Please enter a text with a maximum of ${printableRuleValue} characters`;
+				break;
+			case types.TYPE_DATE:
+				_evalValue=evalValue;
+				ruleMessage=`Please enter a date earlier or equal to ${printableRuleValue}`;
+				break;
+			default:
+				_evalValue=evalValue;
+				ruleMessage=`Maximum allowed value is ${printableRuleValue}`;
+		}
+		return (_evalValue <= ruleValue)?null:ruleMessage;
 	}
 }
 
