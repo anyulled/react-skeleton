80edc2809f6d7b12ceeef171a465b1ccda54ebc0
Pereira Fraga <eypa@gft.com>
2016-10-31 11:46:28 +0100

    DIS-12 edigley.pereira Added visual integration with DataGrid for the Pagination Component

--- client/app/components/DataGrid/Pagination/CustomPagination.js
+++ client/app/components/DataGrid/Pagination/CustomPagination.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Pagination } from "react-bootstrap";
+import { Grid,Row,Col } from "react-bootstrap";
 
 import PageSummary from "../../../containers/DataGrid/Pagination/PageSummaryContainer";
 
@@ -21,20 +22,30 @@ class CustomPagination extends React.Component {
         let  lastElementOfThisPage = ((this.props.pageNumber  ) * this.props.pageSize);
         let totalOfElements = this.props.totalOfElements
         return(
-            <div>
-                <Pagination
-                    prev
-                    next
-                    first
-                    last
-                    ellipsis
-                    boundaryLinks
-                    maxButtons={this.props.maxButtons}
-                    items={this.props.numberOfPages}
-                    activePage={this.props.pageNumber}
-                    onSelect={this.handleSelectPage}
-                />
-                <PageSummary {...this.props}/>
+            <div style={{width:100+"%","backgroundImage":"linear-gradient(#fff,#efefef)","border": "1px solid #d3d3d3","padding":"7px 2px 3px"}}>
+                <Grid fluid>
+                    <Row>
+                        <Col xs={9} sm={10} style={{"paddingLeft":"3px"}}>
+                            <Pagination
+                                prev
+                                next
+                                first
+                                last
+                                ellipsis
+                                boundaryLinks
+                                maxButtons={this.props.maxButtons}
+                                items={this.props.numberOfPages}
+                                activePage={this.props.pageNumber}
+                                onSelect={this.handleSelectPage}
+                            />
+                        </Col>
+                        <Col xs={3} sm={2} style={{"borderLeft":"1px solid rgb(211, 211, 211)","minHeight":"30px","padding":"13px 2px 2px 14px"}}>
+                            <div style={{"border":"1px solid #d3d3d3","padding":"5px 2px","margin":"1px 0px","border-radius":"3px","border-color":"rgb(209,217,222)","color":"#1d769f"}}>
+                                <PageSummary {...this.props}/>
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
 		);
     }
--- client/app/components/DataGrid/Pagination/PageSummary.js
+++ client/app/components/DataGrid/Pagination/PageSummary.js
@@ -3,14 +3,11 @@ import React from "react";
 class PageSummary extends React.Component {	
 
     render() {
-        console.log(this.props);
         let firstElementOfThisPage = ((this.props.pageNumber-1) * this.props.pageSize) + 1;
         let  lastElementOfThisPage = ((this.props.pageNumber  ) * this.props.pageSize);
         let totalOfElements = this.props.totalOfElements
-        return(
-            <div>
+        return(        
                 <span>{firstElementOfThisPage + "-" + lastElementOfThisPage + " of " + totalOfElements}</span>	
-            </div>
 		);
     }
 }
