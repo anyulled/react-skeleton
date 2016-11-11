2da43a5c2d662403ff2d5860bcc6acb7958a7f3c
Alberto Hornés <alberto.hornes@gft.com>
2016-11-11 09:17:21 +0100

    DIS-30 Replaced the object.assign function with the spread operator
    Task-Url: https://agema.gft.com/jira/browse/DIS-30

--- client/app/reducers/reduxSample/reduxSample.js
+++ client/app/reducers/reduxSample/reduxSample.js
@@ -3,20 +3,37 @@ import * as actions from "../../actions/reduxSample/reduxSample.js";
 const initialState = {
         fetching: false,
         name: "Button default name",
-        style: "primary"
+        style: "primary",
+        error: ""
 }
 
+//function reduxSample(state = initialState, action) {
+//    switch (action.type) {
+//        case actions.REQUEST_INFO:
+//            return Object.assign({}, state, { fetching: true });
+//        case actions.RECEIVE_INFO:
+//            return Object.assign({}, state, { fetching: false }, action.info );
+//        case actions.ERROR:
+//            return state;
+//    }
+//    return state;
+//}
+
 function reduxSample(state = initialState, action) {
+    if (!action || !action.type) {
+        return state;
+    }
+    
     switch (action.type) {
         case actions.REQUEST_INFO:
-            return Object.assign({}, state, { fetching: true });
+            return {...state, fetching: true};
         case actions.RECEIVE_INFO:
-            return Object.assign({}, state, { fetching: false }, action.info );
+            return {...state, fetching: false, ...action.info};
         case actions.ERROR:
+            return {...state, fetching: false, ...action.error};
+        default:
             return state;
     }
-
-    return state;
 }
 
 export default reduxSample;
