c7c9ae499a123fe49f09484dbdd5804204da7eae
alrs <anyul.rivas@gft.com>
2016-11-28 10:18:40 +0100

    updating docs.

--- server/app/socket_server/app.js
+++ server/app/socket_server/app.js
@@ -3,29 +3,28 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var users = require('./initial/users.json');
-if(!users){
-	users=[];
+if (!users) {
+    users = [];
 }
 var nextUserId = 1;
 
-io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on("user/add", user=>{
-		console.log("Received new user");
-		console.log(message);
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on("user/add", user => {
+        console.log("Received new user");
+        console.log(message);
 //		users=[...users,user];	  
-	});
-	socket.on("user/load", message=>{
-		console.log("Getting users list");
-		socket.emit("user/load", users); 	  
-	});
+    });
+    socket.on("user/load", message => {
+        console.log("Getting users list");
+        socket.emit("user/load", users);
+    });
 });
 
 
-
-app.get('/', function(req, res){
-	  res.sendFile(__dirname+'/index.html');
-	});
-http.listen(3001, function(){
-  console.log('socket listening on port 3001');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+http.listen(3001, function () {
+    console.log('socket listening on port 3001');
 });
--- server/app/static_server/app.js
+++ server/app/static_server/app.js
@@ -18,7 +18,7 @@ app.get("/users", function (req, res) {
         "Content-Type": "application/json"
     });
     url = require('url');
-    var query = url.parse(req.url,true).query;
+    var query = url.parse(req.url, true).query;
     console.log("Args: " + JSON.stringify(query));
     var pageNumber = req.param('pageNumber');
     var pageSize = req.param('pageSize');
@@ -26,12 +26,12 @@ app.get("/users", function (req, res) {
     pageSize = (pageSize != null) ? pageSize : 13;
     //it paginates only to the 10 existing user files for page size 13
     //or the 6 existing user files for page size 20
-    if (pageSize==13) {
+    if (pageSize == 13) {
         pageNumber = Math.min(pageNumber, 10);
-    } else  if (pageSize==20){
+    } else if (pageSize == 20) {
         pageNumber = Math.min(pageNumber, 7);
     }
-    const fileName = "users"+ pageNumber + "_" + pageSize +".json";
+    const fileName = "users" + pageNumber + "_" + pageSize + ".json";
     res.sendFile(__dirname + resourcePath + fileName);
 });
 
